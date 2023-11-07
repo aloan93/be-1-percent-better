c32269b05a14bf31ded8bdfd454448440a82abd5
libertyskies <libertyemerson@outlook.com>
2023-11-07 11:13:51 +0000

    creating a get user by user id endpoint

--- api/schema.py
+++ api/schema.py
@@ -9,8 +9,14 @@ class UserType(DjangoObjectType):
 class Query(graphene.ObjectType):
 
     get_all_users = graphene.List(UserType)
+    get_user_by_id = graphene.Field(UserType, user_id=graphene.Int())
+
 
     def resolve_get_all_users(self, info):
-            return User.objects.all()
+        return User.objects.all()
     
+    def resolve_get_user_by_id(self, info, user_id):
+        return User.objects.get(pk=user_id)
+         
+
 schema = graphene.Schema(query=Query)

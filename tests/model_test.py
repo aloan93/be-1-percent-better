b9d4f3af537c5defa6639e1bf018e3bfb33b0fd8
libertyskies <libertyemerson@outlook.com>
2023-11-07 15:20:30 +0000

    making a test for the get all exercises endpoint

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,12 +1,12 @@
 import pytest
-from api.models import User
+import api.models
 
 @pytest.mark.django_db
 def test_user_model():
     # Creates a new user in the database
-    user = User.objects.create(username='testuser')
+    user = api.models.User.objects.create(username='testuser')
     # Fetch the User instance from the database 
-    queried_user = User.objects.get(username='testuser')
+    queried_user = api.models.User.objects.get(username='testuser')
     # Assertions check the username matches the expacted value
     assert user.username == 'testuser'
     assert queried_user.username == 'testuser'
--- tests/schema_test.py
+++ tests/schema_test.py
@@ -1,6 +1,6 @@
 from graphene.test import Client
 from api.schema import schema
-from api.models import User
+from api.models import User, Exercise
 import pytest
 
 @pytest.mark.django_db
@@ -19,7 +19,7 @@ def test_get_all_users():
         }
     '''
     
-    # Initialise this client with ethe schema 
+    # Initialise this client with the schema 
     client = Client(schema)
     # execute() method of the client is then called with the query, which sends the query to the GraphQL API.
     executed = client.execute(query)
@@ -33,3 +33,51 @@ def test_get_all_users():
             ]
         }
     }
+
+@pytest.mark.django_db
+def test_get_all_exercises():
+    user = User.objects.create(username='testuser')
+
+    exercise1 = Exercise.objects.create(user_id=user, external_exercise_id='1234', external_exercise_name='Squat', external_exercise_bodypart='Lower Legs', personal_best=0)
+    exercise2 = Exercise.objects.create(user_id=user, external_exercise_id='5678', external_exercise_name='Bicep curl', external_exercise_bodypart='Upper Arms', personal_best=0)
+
+    query = '''
+        query {
+            getAllExercises {
+                exerciseId
+                externalExerciseId
+                externalExerciseName
+                externalExerciseBodypart
+                personalBest
+                userId {
+                    userId
+                    username
+                }
+            }
+        }
+    '''
+    client = Client(schema)
+    executed = client.execute(query)
+    assert executed == {
+        'data': {
+            'getAllExercises': [
+                {
+                    'exerciseId': str(exercise1.exercise_id),
+                    'externalExerciseId': exercise1.external_exercise_id,
+                    'externalExerciseName': exercise1.external_exercise_name,
+                    'externalExerciseBodypart': exercise1.external_exercise_bodypart,
+                    'personalBest': exercise1.personal_best,
+                    'userId': {'userId': str(user.user_id), 'username': user.username}
+                },
+                {
+                    'exerciseId': str(exercise2.exercise_id),
+                    'externalExerciseId': exercise2.external_exercise_id,
+                    'externalExerciseName': exercise2.external_exercise_name,
+                    'externalExerciseBodypart': exercise2.external_exercise_bodypart,
+                    'personalBest': exercise2.personal_best,
+                    'userId': {'userId': str(user.user_id), 'username': user.username}
+                }
+            ]
+        }
+    }
+
